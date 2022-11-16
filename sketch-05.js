79b0ce9c05561c38a8bc81f366070150644baa41
Kumar Saurabh <kumar.saurabh@vernost.in>
2022-11-16 14:22:58 +0530

    frank code

---
+++ sketch-05.js
@@ -0,0 +1,53 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+let text = 'A';
+let fontSize = 1200;
+let fontFamily = 'serif';
+let manager;
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+    context.font = `${fontSize}px ${fontFamily}`;
+    context.textBaseline = 'top';
+    // context.textAlign = 'center';
+
+    const metrics = context.measureText(text);
+    const mx = metrics.actualBoundingBoxLeft * - 1;
+    const my = metrics.actualBoundingBoxAscent * -1;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    const x = (width - mw) * 0.5 - mx;
+    const y = (height - mh) * 0.5 - my;
+
+    context.save();
+    context.translate(x, y);
+
+    context.beginPath();
+    context.rect(mx, my, mw, mh);
+    context.stroke();
+
+    context.fillText(text, 0, 0);
+    context.restore();
+  };
+};
+
+const onKeyUp = function (e) {
+  text = e.key.toUpperCase();
+  manager.render();   
+}
+
+document.addEventListener('keyup', onKeyUp);
+
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+}
+
+start();
